846688f93f0e854aa0ba99effc2e0e15a39a2403
acilshops <putriviliani8@gmail.com>
2025-09-02 23:32:55 +0700

    Update running.sh

--- menu/running.sh
+++ menu/running.sh
@@ -32,7 +32,7 @@ checking_sc() {
         echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
         echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
         echo -e "             \033[0;33mContact Admin :${NC}"
-        echo -e "     \033[0;36mTelegram${NC}: https:t.me/ wa.me/+"
+        echo -e "     \033[0;36mTelegram${NC}: https:t.me/AcilOffcial wa.me/+6287733994799
         echo -e "$COLOR1╰═════════════════════════════════════════════════╯${NC}"
         exit
     fi
