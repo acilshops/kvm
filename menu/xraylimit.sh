7aa6ebaf2f0b4a0e8f26c4f0c3f01c36ce7186a4
acilshops <putriviliani8@gmail.com>
2025-09-03 22:57:58 +0700

    Update xraylimit.sh

--- menu/xraylimit.sh
+++ menu/xraylimit.sh
@@ -23,7 +23,7 @@ echo -e "            ${RED}PERMISSION DENIED !${NC}"
 echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
 echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
 echo -e "             \033[0;33mContact Your Admin ${NC}"
-echo -e "     \033[0;36mTelegram${NC}: t.me/ wa.me/+"
+echo -e "     \033[0;36mTelegram${NC}: t.me/AcilOffcial wa.me/+6287733994799"
 echo -e "$COLOR1└─────────────────────────────────────────────────┘${NC}"
 exit
 fi
